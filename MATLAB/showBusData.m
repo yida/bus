e546e937933017916f0b224672dd40e6c8b4a95d
Yida Zhang <hitzhangyd@gmail.com>
2012-11-29 15:25:14 -0500

    show time stamp as title

--- MATLAB/showBusData.m
+++ MATLAB/showBusData.m
@@ -9,9 +9,9 @@ addpath('ReceiveImu');
 
 % timeStamp, time, Lat, NS, Lon, EW, Alti, r, p ,y, Gx, Gy, Gz, Ax, Ay, Az, Label
 tStamp = syncData(1,:);
-r = syncData(8,:);
-p = syncData(9,:);
-y = syncData(10,:);
+r  = syncData(8,:);
+p  = syncData(9,:);
+y  = syncData(10,:);
 Gx = syncData(11,:);
 Gy = syncData(12,:);
 Gz = syncData(13,:);
@@ -19,10 +19,10 @@ Ax = syncData(14,:);
 Ay = syncData(15,:);
 Az = syncData(16,:);
 
-Sample = 400;
-
-R = rotz(y(1,Sample))*roty(p(1,Sample))*rotx(r(1,Sample))
-rotplot(R(1:3,1:3));
+for Sample = 1 : size(tStamp,2) 
+  R = rotz(y(1,Sample))*roty(p(1,Sample))*rotx(r(1,Sample));
+  rotplot(R(1:3,1:3), tStamp(1,Sample));
+end
 
 % Task 1 : Make time variant plot, like a animation 
 
