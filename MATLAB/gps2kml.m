6b99a2ba72aad83fc5deb4878288df323991728f
Yida Zhang <hitzhangyd@gmail.com>
2012-12-17 11:56:04 -0500

    add more points

--- MATLAB/gps2kml.m
+++ MATLAB/gps2kml.m
@@ -16,39 +16,43 @@ DocDesNode = docNode.createElement('description');
 DocDesNode.appendChild(docNode.createTextNode(sprintf('GPS Data visualization for Data set 1')));
 DocumentElement.appendChild(DocDesNode);
 
-PlacemarkNode = docNode.createElement('Placemark');
-DocumentElement.appendChild(PlacemarkNode);
 
-  PMNameNode = docNode.createElement('name');
-  PMNameNode.appendChild(docNode.createTextNode(...
-            sprintf('name for point')));
-  PMDesNode = docNode.createElement('description');
-  PMDesNode.appendChild(docNode.createTextNode(...
-            sprintf('description for point')));
 
-  PointNode = docNode.createElement('Point');
+for datacounter = 10000 : 10 : 10200
+  PlacemarkNode = docNode.createElement('Placemark');
   
-    extrudeModeNode = docNode.createElement('extrude');
-    extrudeModeNode.appendChild(docNode.createTextNode(...
-            sprintf('1')));
-    PointNode.appendChild(extrudeModeNode);
+    PMNameNode = docNode.createElement('name');
+    PMNameNode.appendChild(docNode.createTextNode(...
+              sprintf('name for point')));
+    PMDesNode = docNode.createElement('description');
+    PMDesNode.appendChild(docNode.createTextNode(...
+              sprintf('description for point')));
+  
+    PointNode = docNode.createElement('Point');
     
-    altitudeModeNode = docNode.createElement('altitudeMode');
-    altitudeModeNode.appendChild(docNode.createTextNode(...
-            sprintf('relativeToGround')));
-    PointNode.appendChild(altitudeModeNode);
-    
-    CoordinatesNode = docNode.createElement('coordinates');
-    datacounter = 345;
-    [lat, lnt] = nmea2degree(LatLnt{datacounter}{3}, LatLnt{datacounter}{4},...
-                              LatLnt{datacounter}{5}, LatLnt{datacounter}{6});
-    CoordinatesNode.appendChild(docNode.createTextNode(...
-            sprintf('%f,%f,%f', lnt, lat, 0)));
-  PointNode.appendChild(CoordinatesNode);
+      extrudeModeNode = docNode.createElement('extrude');
+      extrudeModeNode.appendChild(docNode.createTextNode(...
+              sprintf('1')));
+      PointNode.appendChild(extrudeModeNode);
+      
+      altitudeModeNode = docNode.createElement('altitudeMode');
+      altitudeModeNode.appendChild(docNode.createTextNode(...
+              sprintf('relativeToGround')));
+      PointNode.appendChild(altitudeModeNode);
+      
+      CoordinatesNode = docNode.createElement('coordinates');
+      [lat, lnt] = nmea2degree(LatLnt{datacounter}{3}, LatLnt{datacounter}{4},...
+                                LatLnt{datacounter}{5}, LatLnt{datacounter}{6});
+      CoordinatesNode.appendChild(docNode.createTextNode(...
+              sprintf('%f,%f,%f', lnt, lat, 0)));
+    PointNode.appendChild(CoordinatesNode);
+  
+  PlacemarkNode.appendChild(PMNameNode);
+  PlacemarkNode.appendChild(PMDesNode);
+  PlacemarkNode.appendChild(PointNode);
 
-PlacemarkNode.appendChild(PMNameNode);
-PlacemarkNode.appendChild(PMDesNode);
-PlacemarkNode.appendChild(PointNode);
+  DocumentElement.appendChild(PlacemarkNode);
+end
 
 docNode.appendChild(docNode.createComment('this is a comment'));
 
