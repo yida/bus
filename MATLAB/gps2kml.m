b13c415b9c9da4471f57b385ab2f3a5003486f5f
Yida Zhang <hitzhangyd@gmail.com>
2012-12-17 11:31:43 -0500

    add point name and description tag

--- MATLAB/gps2kml.m
+++ MATLAB/gps2kml.m
@@ -19,27 +19,36 @@ DocumentElement.appendChild(DocDesNode);
 PlacemarkNode = docNode.createElement('Placemark');
 DocumentElement.appendChild(PlacemarkNode);
 
-PointNode = docNode.createElement('Point');
+  PMNameNode = docNode.createElement('name');
+  PMNameNode.appendChild(docNode.createTextNode(...
+            sprintf('name for point')));
+  PMDesNode = docNode.createElement('description');
+  PMDesNode.appendChild(docNode.createTextNode(...
+            sprintf('description for point')));
+
+  PointNode = docNode.createElement('Point');
+  
+    extrudeModeNode = docNode.createElement('extrude');
+    extrudeModeNode.appendChild(docNode.createTextNode(...
+            sprintf('1')));
+    PointNode.appendChild(extrudeModeNode);
+    
+    altitudeModeNode = docNode.createElement('altitudeMode');
+    altitudeModeNode.appendChild(docNode.createTextNode(...
+            sprintf('relativeToGround')));
+    PointNode.appendChild(altitudeModeNode);
+    
+    CoordinatesNode = docNode.createElement('coordinates');
+    datacounter = 345;
+    [lat, lnt] = nmea2degree(LatLnt{datacounter}{3}, LatLnt{datacounter}{4},...
+                              LatLnt{datacounter}{5}, LatLnt{datacounter}{6});
+    CoordinatesNode.appendChild(docNode.createTextNode(...
+            sprintf('%f,%f,%f', lnt, lat, 0)));
+  PointNode.appendChild(CoordinatesNode);
+
+PlacemarkNode.appendChild(PMNameNode);
+PlacemarkNode.appendChild(PMDesNode);
 PlacemarkNode.appendChild(PointNode);
-
-
-extrudeModeNode = docNode.createElement('extrude');
-extrudeModeNode.appendChild(docNode.createTextNode(...
-        sprintf('1')));
-PointNode.appendChild(extrudeModeNode);
-
-altitudeModeNode = docNode.createElement('altitudeMode');
-altitudeModeNode.appendChild(docNode.createTextNode(...
-        sprintf('relativeToGround')));
-PointNode.appendChild(altitudeModeNode);
-
-CoordinatesNode = docNode.createElement('coordinates');
-datacounter = 345;
-[lat, lnt] = nmea2degree(LatLnt{datacounter}{3}, LatLnt{datacounter}{4},...
-                          LatLnt{datacounter}{5}, LatLnt{datacounter}{6});
-CoordinatesNode.appendChild(docNode.createTextNode(...
-        sprintf('%f,%f,%f', lnt, lat, 0)));
-PointNode.appendChild(CoordinatesNode);
 
 docNode.appendChild(docNode.createComment('this is a comment'));
 
