e75218d4140ecc9ea5f30a452fd4ded79dcbcbad
Yida Zhang <hitzhangyd@gmail.com>
2012-12-18 15:05:12 -0500

    add two walls

--- MATLAB/lidar/lidar_simu.m
+++ MATLAB/lidar/lidar_simu.m
@@ -1,19 +1,34 @@
 clear all;
 close all;
 
-busCenter = [0.5, 1.9];
+%busCenter = [0.5, 1.9];
+busCenter = [0, 1.5];
 busTheta = pi/2;
 busTheta = pi/12*5;
+
 
 for busTheta = 0 : pi/100 : pi/2 
 %busTheta = 0;
 
   [axisRange, Ver] = plot_bus(busCenter, busTheta);
   hold on;
-  
-  axisGain = 1.3;
+
+  % line 1
+  plot([-3, 3], [2.8, 2.8]);
+  % line 2
+  plot([-1, -1], [0, 4]);
+  % circle 1
+  nseg = 60;
+  theta = 0 : (2 * pi / nseg) : (2 * pi);
+  cx = 0.5; cy = 2.1; r = 0.02;
+  pline_x = r * cos(theta) + cx;
+  pline_y = r * sin(theta) + cy;
+  plot(pline_x, pline_y, 'k');
     
-  axis(axisRange.*[1/axisGain, axisGain, 1/axisGain, axisGain]);
+  axisGain = 3.3;
+    
+%  axis(axisRange.*[1/axisGain, axisGain, 1/axisGain, axisGain]);
+  axis([-1.5, 1, 0, 4]);
   
   plot_lidar(busTheta, Ver(1,:));
   plot_lidar(busTheta - 3/2*pi, Ver(2,:));
--- MATLAB/lidar/plot_lidar.m
+++ MATLAB/lidar/plot_lidar.m
@@ -2,21 +2,55 @@ function plot_lidar(lidarTheta, lidarCenter)
 
   numBeams = 1081;
   beamRange = 3/2*pi;
-  lidarMaxRange = 0.5;
+  MaxRange = 1;
+  lidarMaxRange = MaxRange * ones(1, numBeams);
 
   lidarTheta = lidarTheta + beamRange; 
 
   lidarAngles = beamRange/numBeams : beamRange/numBeams : beamRange;
   lidarAngles = lidarAngles + lidarTheta;
-  lidar = [lidarMaxRange * cos(lidarAngles); lidarMaxRange * sin(lidarAngles)];
+
+  % line 1
+  lidarRange = (2.8 - lidarCenter(2))./sin(lidarAngles);
+  lidarRange(lidarRange > MaxRange) = MaxRange;
+  lidarRange(lidarRange < 0) = MaxRange;
+
+  % line 2
+  lidarRange1 = (-1 - lidarCenter(1))./cos(lidarAngles);
+  lidarRange1(lidarRange1 > MaxRange) = MaxRange;
+  lidarRange1(lidarRange1 < 0) = MaxRange;
+
+  lidarRange = min(lidarRange, lidarRange1);
+
+  % Circle 1
+%  sqrtroot = 4.*((lidarCenter(1)-0.5).*cos(lidarAngles) + (lidarCenter(2)-2.1).*sin(lidarAngles)).^2 -...
+%  4.*((lidarCenter(1)-0.5).^2+(lidarCenter(2)-2.1).^2 + 0.02^2);
+%  B = cos(lidarAngles) + sin(lidarAngles);
+%  Bsign = B ./ abs(B);
+%  nonsectRayIdx = find(sqrtroot < 0);
+%  sectRayIdx = find(sqrtroot >= 0);
+%  sqrtroot(nonsectRayIdx) = 0;
+%  sqrtroot(sectRayIdx) = sqrt(sqrtroot(sectRayIdx));
+%  sqrtroot
+%  lidarRange2 = -B -Bsign .* sqrtroot / 2; 
+%  lidarRange2(lidarRange2 > MaxRange) = MaxRange;
+%  lidarRange2(lidarRange2 < 0) = MaxRange;
+
+  
+
+  lidar = [lidarMaxRange .* cos(lidarAngles); lidarMaxRange .* sin(lidarAngles)];
+  lidar1 = [lidarRange .* cos(lidarAngles); lidarRange .* sin(lidarAngles)];
   lidar = bsxfun(@plus, lidar, lidarCenter');
+  lidar1 = bsxfun(@plus, lidar1, lidarCenter');
 
   idx = 1:5:numBeams;
 
-  plot(lidar(1,idx), lidar(2,idx), 'w*');
+%  plot(lidar(1,idx), lidar(2,idx), 'w*');
+  plot(lidar1(1,idx), lidar1(2,idx), 'w*');
 
   for cnt = idx
-    plot([lidarCenter(1,1), lidar(1,cnt)], [lidarCenter(1,2), lidar(2,cnt)], 'r');
+%    plot([lidarCenter(1,1), lidar(1,cnt)], [lidarCenter(1,2), lidar(2,cnt)], 'r');
+    plot([lidarCenter(1,1), lidar1(1,cnt)], [lidarCenter(1,2), lidar1(2,cnt)], 'r');
   end
   
   
