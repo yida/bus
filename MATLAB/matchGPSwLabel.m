d15465e517b39df68c33b39e67b5c9dfa5574e82
Yida Zhang <yida@seas.upenn.edu>
2012-12-20 17:54:46 -0500

    match label time stamp with the closet gps point

--- MATLAB/matchGPSwLabel.m
+++ MATLAB/matchGPSwLabel.m
@@ -1,14 +1,22 @@
 sizeLabel = size(label, 2);
 sizeGPS = size(LatLnt, 2);
 
-labelt = label{1}.tstamp;
-gpst = LatLnt{1}[2];
-
 gpscount = 1;
+labelcount = 1;
 labelt = label{1}.tstamp;
-gpst = LatLnt{gpscount}[2];
+gpst = LatLnt{gpscount}{2};
 
-while (labelt > gpst)
+for labelcount = 1 : sizeLabel
+  labelt = label{labelcount}.tstamp;
+  gpst = LatLnt{gpscount}{2};
 
+  lastTdiff = 1000;
+  while (abs(labelt - gpst) < lastTdiff) & (gpscount < sizeGPS)
+    lastTdiff = abs(labelt - gpst);
+    gpscount = gpscount + 1;
+    gpst = LatLnt{gpscount}{2};
+  end
+  
+  fprintf(1, 'min time diff to label %d: %f at GPS frame %d\n',...
+        labelcount, labelt-LatLnt{gpscount-1}{2}, gpscount);
 end
-
