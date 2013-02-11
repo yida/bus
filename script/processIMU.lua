bb682087b6cc8836d95f7fb95c1f16fccfe54bc2
Yida Zhang <hitzhangyd@gmail.com>
2013-02-11 11:37:03 -0800

    remove test.xml and add maxline example

--- script/processIMU.lua
+++ script/processIMU.lua
@@ -1,11 +1,10 @@
 require 'include'
 require 'common'
 
-local datasetpath = '../data/dataset9/'
-local imuset = loadData(datasetpath, 'imu')
+local datasetpath = '../data/010213180247/'
+local imuset = loadData(datasetpath, 'imu', 1000)
 
-
-imuPruned = pruneTUC(imuset)
+--imuPruned = pruneTUC(imuset)
 
 --saveData(imuPruned, 'imuPruned')
 
