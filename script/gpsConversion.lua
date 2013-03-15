f42ba72892a522a81c5ab30475efcd92dc6c06ce
Yida Zhang <hitzhangyd@gmail.com>
2013-03-14 21:31:41 -0400

    smear dop on all gps points

--- script/gpsConversion.lua
+++ script/gpsConversion.lua
@@ -8,12 +8,12 @@ require 'GPSUtils'
 
 local serialization = require('serialization');
 
---local datasetpath = '../data/150213185940/'
+local datasetpath = '../data/150213185940/'
 --local datasetpath = '../data/010213180247/'
 --local datasetpath = '../data/010213192135/'
 --local datasetpath = '../data/191212190259/'
 --local datasetpath = '../data/211212164337/'
-local datasetpath = '../data/211212165622/'
+--local datasetpath = '../data/211212165622/'
 local dataset = loadData(datasetpath, 'gps', _, 1)
 
 earth = Geocentric.new(Constants.WGS84_a(), Constants.WGS84_f())
@@ -22,40 +22,106 @@ coorInit = false
 local counter = 0
 local labelcounter = 0
 local gpsLocal = {}
+local dopcounter = 0
+local HDOP = '0'
+local VDOP = '0*06'
+local PDOP = '0'
+local height = 0
+local heightInit = false
+local latitude = 0
+local latitudeInit = false
+local longtitude = 0
+local longtitudeInit = false
+local tstep = 0
+local datatype = ''
+local coorInit = false
+
 for i = 1, #dataset do
   if dataset[i].type == 'gps' then
-    if dataset[i].latitude ~= nil and dataset[i].longtitude ~= nil and dataset[i].height ~= nil and
-       dataset[i].latitude ~= '' and dataset[i].longtitude ~= '' and dataset[i].height ~= '' then
-      local lat, lnt = nmea2degree(dataset[i].latitude, dataset[i].northsouth, 
-                                  dataset[i].longtitude, dataset[i].eastwest)
---      local gpspos = global2metric(dataset[i])
-      local lgps = {}
-      lgps.timestamp = dataset[i].timstamp or dataset[i].timestamp
-      lgps.type = dataset[i].type
-      if not coorInit then  
-        proj = LocalCartesian.new(lat, lnt, dataset[i].height, earth)
-        coorInit = true
-      else
-        ret = proj:Forward(lat, lnt, dataset[i].height)
---        print(ret.x, ret.y, ret.z)
-        lgps.x = ret.x
-        lgps.y = ret.y
-        lgps.z = ret.z
-        local tstep = dataset[i].timestamp
-        
-        counter = counter + 1         
-        gpsLocal[#gpsLocal+1] = lgps
---        pos = vector.new({ret.x, ret.y, ret.z})
---        pos = vector.new({gpspos[1], gpspos[2], gpspos[3]})
---        ucm.set_ukf_counter(counter)
---        ucm.set_ukf_pos(pos)
-      end
-      gpsstr = serialization.serialize(lgps)
-      print(gpsstr)
---      usleep(0.01)
+    local positionUpdate = false
+    if dataset[i].HDOP ~= nil then
+      HDOP = tonumber(dataset[i].HDOP)
     end
+    if dataset[i].VDOP ~= nil then
+      VDOP = tonumber(dataset[i].VDOP:sub(1, #dataset[i].VDOP-3))
+    end
+    if dataset[i].PDOP ~= nil then
+      PDOP = tonumber(dataset[i].PDOP)
+    end
+
+    if dataset[i].height ~= nil and dataset[i].height ~= '' then
+      height = dataset[i].height
+      heightInit = true
+    end
+
+    if dataset[i].latitude ~= nil and dataset[i].longtitude ~= nil and
+       dataset[i].latitude ~= '' and dataset[i].longtitude ~= '' then
+      latitudeInit = true
+      longtitudeInit = true
+      positionUpdate = true
+      latitude, longtitude = nmea2degree(dataset[i].latitude, dataset[i].northsouth, 
+                                  dataset[i].longtitude, dataset[i].eastwest)
+    end
+
+    local timestamp = dataset[i].timstamp or dataset[i].timestamp
+    if timestamp ~= nil then tstep = timestamp end
+    if dataset[i].type ~= nil then datatype = dataset[i].type end
+    
+--    print(latitude, longtitude, height)
+    if coorInit == false and heightInit and latitudeInit and longtitudeInit then
+      proj = LocalCartesian.new(latitude, longtitude, height, earth)
+      coorInit = true
+    end
+
+    if coorInit and positionUpdate then
+      ret = proj:Forward(latitude, longtitude, height)
+      print(ret.x, ret.y, ret.z, HDOP, VDOP, PDOP, latitude, longtitude)
+      dopcounter = dopcounter + 1
+      local lgps = {}
+      lgps.timestamp = timestamp
+      lgps.type = datatype
+      lgps.x = ret.x
+      lgps.y = ret.y
+      lgps.z = ret.z
+      lgps.latitude = latitude
+      lgps.longtitude = longtitude
+      lgps.HDOP = ret.HDOP
+      lgps.VDOP = ret.VDOP
+      lgps.PDOP = ret.PDOP
+      gpsLocal[#gpsLocal+1] = lgps
+      gpsstr = serialization.serialize(lgps)
+--      print(gpsstr)
+
+    end
+
+--    if dataset[i].latitude ~= nil and dataset[i].longtitude ~= nil and
+--       dataset[i].latitude ~= '' and dataset[i].longtitude ~= '' then
+--      local lat, lnt = nmea2degree(dataset[i].latitude, dataset[i].northsouth, 
+--                                  dataset[i].longtitude, dataset[i].eastwest)
+----      local gpspos = global2metric(dataset[i])
+--      local lgps = {}
+--      lgps.timestamp = dataset[i].timstamp or dataset[i].timestamp
+--      lgps.type = dataset[i].type
+--      if not coorInit then  
+--        proj = LocalCartesian.new(lat, lnt, dataset[i].height, earth)
+--        coorInit = true
+--      else
+--        ret = proj:Forward(lat, lnt, dataset[i].height)
+----        print(ret.x, ret.y, ret.z)
+--        lgps.x = ret.x
+--        lgps.y = ret.y
+--        lgps.z = ret.z
+--        local tstep = dataset[i].timestamp
+--        
+--        counter = counter + 1         
+--        gpsLocal[#gpsLocal+1] = lgps
+--      end
+--      gpsstr = serialization.serialize(lgps)
+--    --  print(gpsstr)
+----      usleep(0.01)
+--    end
   end
 end
 
-print(#gpsLocal)
-saveData(gpsLocal, 'gpsLocal')
+print(#gpsLocal, dopcounter)
+--saveData(gpsLocal, 'gpsLocal')
--- script/include.lua
+++ script/include.lua
@@ -3,9 +3,9 @@ local repopath = '../../UPennTHOR/Player'
 package.path = repopath..'/Util/?.lua;'..package.path
 package.cpath = repopath..'/Lib/?.so;'..package.cpath
 
-package.cpath = pwd..'lib/GeographicLib/?.so;'..package.cpath
-package.cpath = pwd..'lib/Serial/?.so;'..package.cpath
-package.cpath = pwd..'lib/LuaXml/?.so;'..package.cpath
-package.path = pwd..'lib/LuaXml/?.lua;'..package.path
+package.cpath = pwd..'/lib/GeographicLib/?.so;'..package.cpath
+package.cpath = pwd..'/lib/Serial/?.so;'..package.cpath
+package.cpath = pwd..'/lib/LuaXml/?.so;'..package.cpath
+package.path = pwd..'/lib/LuaXml/?.lua;'..package.path
 
 
